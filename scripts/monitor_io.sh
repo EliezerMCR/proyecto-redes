efccf3261110554a343cb6eb704ad34ffb197fcf
degel01459 <15-11661@usb.ve>
2025-11-29 01:07:10 -0400

    io

--- scripts/monitor_io.sh
+++ scripts/monitor_io.sh
@@ -20,8 +20,6 @@ while true; do
     timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
     
     # 2. CAPTURA DE DATOS
-    # Usamos grep para filtrar sda (VirtualBox estándar) o vda (VirtIO)
-    # Si no te sale nada en el CSV, cambia 'sd[a-z]' por 'dm-[0-9]' o el nombre de tu disco.
     stats=$(iostat -dx 1 1 | awk -v t="$timestamp" '/sd[a-z]|vd[a-z]/ {print t","$1","$4","$5","$14}')
     
     # Solo escribir si se encontraron datos para evitar líneas vacías
